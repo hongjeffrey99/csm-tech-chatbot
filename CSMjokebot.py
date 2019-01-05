3a4f50ec6a1bc8a007ce9fe6bd5cebd237ae202b
Jeffrey Hong <hongjeffrey99@gmail.com>
2019-01-05 12:01:02 -0800

    reorganized repo and joke storage format

--- CSMjokebot.py
+++ CSMjokebot.py
@@ -22,22 +22,24 @@ def csvRead(file):
     with open(file, newline='') as f:
         reader = csv.reader(f)
         for row in reader:
-            div = row.split(',')
-            jokes += div
+            #div = row.split(',')
+            #jokes += div
+            jokes += row
     return jokes
 
 # Reads command line argument in format 'python jokebot.py jokes.csv'
 if __name__ == '__main__':
     # Checks if a CSV filename is given
-    if not sys.argv[1] or sys.argv[1].endswith('.csv'):
+    if len(sys.argv) == 1 or not sys.argv[1].endswith('.csv'):
         print("No joke file given")
         quit()
     # Else deliver prompt and punchline, then await userinput
     # First joke is delivered without user input
     jokes = csvRead(sys.argv[1])
-    jokeDeliver(jokes[0][0], jokes[0][1])
+    jokeDeliver(jokes[0], jokes[1])
     # If user input is verified and while there are still jokes, deliver
-    i = 1
+    i = 2
     while userInput() or i == len(jokes):
-        jokeDeliver(jokes[i][0], jokes[i][1])
+        jokeDeliver(jokes[i], jokes[i + 1])
+        i += 2
     quit()
