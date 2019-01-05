98c2ee971cef0f4a12a59b83c7fbee8662697de5
Jeffrey Hong <hongjeffrey99@gmail.com>
2019-01-05 15:27:16 -0800

    version 2 with reading from dadjokes

--- CSMjokebot.py
+++ CSMjokebot.py
@@ -1,12 +1,16 @@
 import time
 import csv
 import sys
+import requests
+import json
 
+# Sleep two seconds between printing prompt and punchline of a joke
 def jokeDeliver(prompt, punchline):
     print(prompt)
     time.sleep(2)
     print(punchline)
 
+# Read userinput for next joke or quitting
 def userInput():
     while True:
         read = input()
@@ -17,29 +21,45 @@ def userInput():
         else:
             print("I don't understand")
 
+# Read csv file formats of prompt,punchline
 def csvRead(file):
     jokes = []
     with open(file, newline='') as f:
         reader = csv.reader(f)
         for row in reader:
-            #div = row.split(',')
-            #jokes += div
             jokes += row
     return jokes
 
-# Reads command line argument in format 'python jokebot.py jokes.csv'
+# Get and filter reddit dadjokes
+def pullJokes():
+    # Get JSON and format it
+    jokesDict = requests.get('https://www.reddit.com/r/dadjokes.json', headers = {'User-agent': 'bot'}).json()
+
+    jokes = []
+    # Check title and over_18 for each tagged post on the page
+    for tag in jokesDict['data']['children']:
+        headers = tag['data']
+        #print(tag['data']['title'])
+        if not headers['over_18'] 
+            and (headers['title'].startswith('Why') or headers['title'].startswith('What') or headers['title'].startswith('How')):
+            jokes += [headers['title'], headers['selftext']]
+    return jokes
+
+# Read command line argument in format 'python jokebot.py jokes.csv'
 if __name__ == '__main__':
-    # Checks if a CSV filename is given
+    # Check if a CSV filename is given, if not then get jokes from non-CSV source
     if len(sys.argv) == 1 or not sys.argv[1].endswith('.csv'):
         print("No joke file given")
-        quit()
-    # Else deliver prompt and punchline, then await userinput
+        jokes = pullJokes()
+    else:
+        jokes = csvRead(sys.argv[1])
     # First joke is delivered without user input
-    jokes = csvRead(sys.argv[1])
+    if not jokes:
+        quit()
     jokeDeliver(jokes[0], jokes[1])
     # If user input is verified and while there are still jokes, deliver
     i = 2
-    while userInput() or i == len(jokes):
+    while userInput() and i + 1 < len(jokes):
         jokeDeliver(jokes[i], jokes[i + 1])
         i += 2
     quit()
